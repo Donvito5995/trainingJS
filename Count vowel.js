4b04b8da07fa3b2c03ae73c8397f99c24a170cf0
Oleksandr Buhai <96393583+Donvito5995@users.noreply.github.com>
2022-02-10 17:11:16 +0100

    Vowel Count

---
+++ Count vowel.js
@@ -0,0 +1,5 @@
+for(let i = 0 ; i <str.length; i++){
+    if(str[i] == "a" || str[i] == "e" || str[i] == "i" || str[i] == "o" || str[i] == "u")
+      {vowelsCount++}}
+      
+      return vowelsCount;
